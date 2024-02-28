4ce544bb5572ef84e7a14b48e68aeb9d869f4e9d
Ever-VC <ever.vasquezc27@gmail.com>
2024-02-28 13:57:40 -0600

    Funciones de cadenas y condicionales

--- cadenas.py
+++ cadenas.py
@@ -85,6 +85,53 @@ print(msg[::-1]) # SALIDA ESPERADA: otxet ed anedac anu se atsE
 
 # ----------------- FUNCIONES DE CADENAS -----------------
 
+# len(): Retorna el tamaño de la cadena
+msg = "Hola mundo"
+print( len(msg)) # SALIDA ESPERADA: 10
+
 # upper(): Convierte una cadena a mayúsculas
 msg = 'hola mundo'
 print(msg.upper()) # SALIDA ESPERADA: HOLA MUNDO
+
+# lower(): Convierte una cadena a minúsculas
+msg = 'HOLA MUNDO'
+print(msg.lower()) # SALIDA ESPERADA: hola mundo
+
+# in: Verifica si una palabra o letra existe en una cadena
+msg = "Hola mundo"
+print("u" in msg) # SALIDA ESPERADA: True
+
+# strip(): Crea una nueva cadena eliminando los espacios que hay en la cadena original
+print( msg.strip()) # SALIDA ESPERADA: Hola mundo
+
+# lstrip(): Crea una nueva cadena eliminando los espacios del INICIO que hay en la cadena original
+# rstrip(): Crea una nueva cadena eliminando los espacios del FINAL que hay en la cadena original
+
+# startswith(): Retorna True o False si la palabra existe al inicio de la cadena
+msg = "Hola Mundo"
+print(msg.startswith("Mundo")) # SALIDA ESPERADA: False
+
+# startswith(): Retorna True o False si la palabra existe al final de la cadena
+print(msg.endswith("Mundo")) # SALIDA ESPERADA: True
+
+#fin(): Retorna la posición de la primera ocurrencia de la palabra o letra buscada en una cadena
+print(msg.find("Mundo")) # SALIDA ESPERADA: 5
+
+# count(): Retorna la cantidad de ocurrencias de la palabra o letra buscada en una cadena
+print(msg.count("2")) # SALIDA ESPERADA: 0
+
+# replace(): Cambia el valor específico de una cadena por otro indicado como parámetro
+#Si hay mayor ocurrencias del primer parámetro, se pone como tercer parámetro la cantidad de veces que se desea reemplezar
+print( msg.replace("mundo", "clase")) # SALIDA ESPERADA: Hola clase
+
+# idigit(): Verifica si el contenido de la cadena es un número
+msg = "3"
+print( msg.isdigit()) # SALIDA ESPERADA: True
+
+# isalpha(): Verifica si el contenido de la cadena son letras (sin espacios o caracteres especiales)
+msg = "mensaje"
+print( msg.isalpha()) # SALIDA ESPERADA: True
+
+# isalnum(): Verifica si el contenido de la cadena son letras o números (sin espacios o caracteres especiales)
+msg = "HolaMundo2"
+print( msg.isalnum()) # SALIDA ESPERADA: True
---
+++ condicionales.py
@@ -0,0 +1,22 @@
+#CONDICIONALES ANIDADOS
+if True:
+    print("Mensaje dentro del condicional 1")
+    if True:
+        print("Mensaje dentro del condicional 2")
+    print("Fuera del condicional 2")
+print("Mensaje fuera del ambos condicionales")
+
+#CONDICIONAL IF ELSE Y ELIF
+if False:
+    print("Condicion if")
+else:
+    print("COndicion else")
+    
+if False:
+    print("Condicion if")
+elif False:
+    print("Primer elif")
+elif False:
+    print("Segundo elif")
+else:
+    print("Ninguna condicion se cumplio")

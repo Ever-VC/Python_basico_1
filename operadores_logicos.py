bf5507976cbd81252068b3f5f5b33cab5fed213a
Ever-VC <ever.vasquezc27@gmail.com>
2024-03-10 15:53:24 -0600

    Operadores logicos

---
+++ operadores_logicos.py
@@ -0,0 +1,21 @@
+"""
+EXPRESIONES BOOLEANAS: Una expresion booleana es aquella que puede ser verdadera True o falsa False
+"""
+#Muestra el tipo de dato al que pertenece una expresion booleana
+print(type(True))# SALIDA ESPERADA: <class 'bool'>
+#Muestra True si 5 es igual que 5
+print(5 == 5)# SALIDA ESPERADA: True
+#Muestra True si 5 es diferente que 6
+print(5 == 6)# SALIDA ESPERADA: False
+#Muestra True si 5 es menor que 6
+print(5 < 6)# SALIDA ESPERADA: True
+#Muestra True si 5 es mayor que 6
+print(5 > 6)# SALIDA ESPERADA: False
+#Muestra True si 5 es menor o igual que 6
+print(5 <= 6)# SALIDA ESPERADA: True
+#Muestra True si 5 es mayor o igual que 6
+print(5 >= 6)# SALIDA ESPERADA: False
+#Muestra True si 5 es diferente que 6
+print(5 != 6)# SALIDA ESPERADA: True
+#Muestra True si 5 es diferente que 5
+print(5 != 5)# SALIDA ESPERADA: False

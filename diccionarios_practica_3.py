3b9a0b9cc0fc68751eb2934277bba0f37e564b95
Ever VC <ever.vasquezc27@gmail.com>
2024-04-16 11:47:44 -0600

    Tercer ejercicio practico con diccionarios

---
+++ diccionarios_practica_3.py
@@ -0,0 +1,25 @@
+'''
+Escribir un programa que guarde en un diccionario los precios de las frutas 
+de la tabla, pregunte al usuario por una fruta, un número de kilos y 
+muestre por pantalla el precio de ese número de kilos de fruta. Si la fruta 
+no está en el diccionario debe mostrar un mensaje informando de ello.
+
+┌------------------┐
+| Fruta   | Precio |
+|---------|--------|
+| Plátano | 1.35   |
+| Manzana | 0.80   |
+| Pera    |	0.85   |
+| Naranja |	0.70   |
+└------------------┘
+'''
+
+frutas = {
+    'Plátano': 1.35,
+    'Manzana': 0.80,
+    'Pera': 0.85,
+    'Naranja': 0.70
+}
+
+fruta = input("Ingrese una fruta: ").title()
+print(frutas.get(fruta, "La fruta no está en el diccionario."))
